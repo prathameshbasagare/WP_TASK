865c405f8335fff9c31c453f790f9272c884a5a5
Prathamesh Basagare <prathamesh.basagare@wisdmlabs.com>
2025-02-24 17:17:41 +0530

    update header

--- themes/my-theme/functions.php
+++ themes/my-theme/functions.php
@@ -2,10 +2,27 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', 'my_theme_styles' );
+add_action( 'wp_enqueue_scripts', 'p_custom_styles' );
 
+add_action('wp_footer', 'my_theme_custom_js');
 function my_theme_styles() {
 	wp_enqueue_style( 
 		'my-theme-style', 
 		get_stylesheet_uri()
 	);
 }
+
+function p_custom_styles(){
+	wp_enqueue_style( 
+		'p-custom-style', 
+		get_theme_file_uri('/assets/css/styles.css')
+	);
+}
+
+function my_theme_custom_js(){
+	wp_enqueue_script(
+		'my-theme-js',
+		get_theme_file_uri('/assets/js/index.js')
+	);
+}
+
