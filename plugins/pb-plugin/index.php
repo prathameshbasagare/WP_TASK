dd3086ce226b88ba9df949da5890566b2161b193
Prathamesh Basagare <prathamesh.basagare@wisdmlabs.com>
2025-02-25 17:47:26 +0530

    Minor Changes

---
+++ plugins/pb-plugin/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Plugin Name: PB PLUGIN
+* Description: Plugin to add custom blocks in site
+* Version: 1.0.0
+* Text Domain: pb-plugin
+* Domain Path: /languages
+*/
+
+if(!function_exists('add_action')){
+    echo "Get the hell out of here";
+    exit;
+}
