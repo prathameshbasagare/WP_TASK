730b7e422c44330feedd3cef6c7e5072578027c0
Prathamesh Basagare <prathamesh.basagare@wisdmlabs.com>
2025-02-26 18:07:48 +0530

    Updated Hero section

--- themes/my-theme/assets/js/index.js
+++ themes/my-theme/assets/js/index.js
@@ -8,3 +8,16 @@ navLinks.forEach(link => {
         nav = link;
     });
 });
+
+const social = document.querySelectorAll('social-icon')
+
+social.forEach(icon=>{
+    icon.classList.add('social-icon-hover')
+    icon.addEventListener("mouseover",()=>{
+        icon.classList.remove('social-icon-hover')
+    })
+
+    icon.addEventListener("mouseout",()=>{
+        icon.classList.add('social-icon-hover')
+    })
+})
